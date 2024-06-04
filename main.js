77dd5b81e5c553e158833ddd82fd2a1536fa4ce3
petter <petternds@gmail.com>
2024-06-04 18:31:32 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocaSom(idElementoAudio) {
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-    document.querySelector(idElementoAudio).play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -16,6 +22,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     
     tecla.onclick = function () {
         tocaSom(idAudio)
-    };
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
     
+}
 }
